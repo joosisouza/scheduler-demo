482723396cb3a01a110da927f9d13028ff16bf50
joosisouza <josi.canobel@gmail.com>
2017-10-02 17:34:59 -0300

    Implementacao do Scheduler

--- src/main.c
+++ src/main.c
@@ -2,13 +2,24 @@
 
 int main() {
   int a[5];
-  int n;
+  int n, k;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
   for (int i=0; i<n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
-  }
+    k=0;
+    for (int j=0; j<5; j++){
+		    if ((i+1)%(a[j]) == 0){
+			        printf("%d",j);
+			        k++;
+		     }
+	  }
+	  if (k == 0){
+  		  printf("-");
+	  }
+    printf("\n");
+	}
 
   return 0;
 }
